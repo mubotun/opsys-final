063cd4c42c5bdc19e36ae5c2628a633d7f3cd5bb
mubotun <39969544+mubotun@users.noreply.github.com>
2019-11-21 21:46:56 -0500

    Client Side

---
+++ ClientWindowListener.java
@@ -0,0 +1,8 @@
+package messengerclient;
+
+public interface ClientWindowListener
+{
+    public void openWindow(String message);
+    public void closeWindow(String message);
+    public void fileStatus(String filesStatus);
+}
