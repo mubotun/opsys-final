4e9015a47c17115c5717794123626273339081e4
mubotun <39969544+mubotun@users.noreply.github.com>
2019-11-21 22:08:38 -0500

    Client Side

--- ClientConstant.java
+++ ClientConstant.java
@@ -1,7 +1,7 @@
 package messengerclient;
 
 public interface ClientConstant
-{
+{ 
     public static final String SERVER_ADDRESS="172.17.0.32";
     public static final int SERVER_PORT=12345;
     public static final int CLIENT_NUMBER=100;
